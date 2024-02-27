6d8c582c4d8373a9f3787bc37523d2242c221e4b
Travis Britz <Travis-Britz@users.noreply.github.com>
2024-02-27 06:20:18 -0600

    define constants for the locales supported by sanctuary

---
+++ sanctuary/constants.go
@@ -0,0 +1,16 @@
+package sanctuary
+
+import "github.com/Travis-Britz/ps2"
+
+const (
+	En ps2.Locale = "en"
+	De ps2.Locale = "de"
+	Es ps2.Locale = "es"
+	Fr ps2.Locale = "fr"
+	It ps2.Locale = "it"
+	Ko ps2.Locale = "ko"
+	Pt ps2.Locale = "pt"
+	Ru ps2.Locale = "ru"
+	Tr ps2.Locale = "tr"
+	Zh ps2.Locale = "zh"
+)
