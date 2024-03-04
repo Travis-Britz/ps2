d18b13df50bbcc9b682b4299c0b57efcf84d3e51
Travis Britz <Travis-Britz@users.noreply.github.com>
2024-03-04 04:55:32 -0600

    add census rate limiting

--- census/client.go
+++ census/client.go
@@ -3,13 +3,21 @@ package census
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
+	"time"
 
 	"github.com/Travis-Britz/ps2"
 )
+
+func init() {
+	RateLimit(5, 2)
+}
+
+var RateLimiter rateLimiter
 
 const apiBase = "https://census.daybreakgames.com"
 
@@ -35,6 +43,14 @@ type Client struct {
 }
 
 func (c Client) Get(ctx context.Context, env ps2.Environment, query string, result any) error {
+	select {
+	case _, ok := <-RateLimiter.Ready():
+		if !ok {
+			return errors.New("rate limiter stopped")
+		}
+	case <-ctx.Done():
+		return ctx.Err()
+	}
 	url := fmt.Sprintf("%s/s:%s/get/%s/%s", apiBase, c.Key, Namespace(env), query)
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
@@ -59,4 +75,41 @@ func (c Client) Get(ctx context.Context, env ps2.Environment, query string, resu
 		return fmt.Errorf("census.Client.Get: UnmarshalJSON: %w", err)
 	}
 	return nil
+}
+
+// RateLimit sets the global rate limiter used by every Client.
+// burst sets the number of requests that can be sent initially without throttling,
+// and nPerSec defines how many requests can be made per second after that.
+//
+// To use a custom rate limiter,
+// set the package RateLimiter variable.
+func RateLimit(burst, nPerSec int) {
+	stopLastLimiter()
+	if burst < 1 {
+		burst = 1
+	}
+	limiter := make(rateLimit, burst-1) //nPerSec-1 because we assume at the start of a burst there will already be a waiting send from ticker
+	ticker := time.NewTicker(time.Second / time.Duration(nPerSec))
+	stopLastLimiter = ticker.Stop
+	RateLimiter = limiter
+	for range burst - 1 {
+		limiter <- struct{}{}
+	}
+	go func() {
+		for range ticker.C {
+			limiter <- struct{}{}
+		}
+	}()
+}
+
+var stopLastLimiter func() = func() {}
+
+type rateLimiter interface {
+	Ready() <-chan struct{}
+}
+
+type rateLimit chan struct{}
+
+func (limit rateLimit) Ready() <-chan struct{} {
+	return limit
 }
